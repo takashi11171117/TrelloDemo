c12f53296b21b7ca815686f0b623087f240c7051
Takashi Saito <takashi11171117@gmail.com>
2019-09-18 11:36:10 +0900

    Used subscriptions

--- src/views/RXSample.vue
+++ src/views/RXSample.vue
@@ -6,27 +6,22 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { interval } from 'rxjs';
+import { interval, Subject } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
-  components: {
-  },
+    subscriptions: () => {
+        return {
+            count: interval(1000).pipe(
+                filter((n: number) => {
+                    return n % 2 !== 0
+                }),
+                map((n: number) => n * n)
+            )
+        }
+    }
 })
 export default class Home extends Vue {
     public count: Number = 0;
-
-    created () {
-        let timer = interval(1000);
-        let timerObserver = timer.pipe(
-            filter((n: number) => {
-                return n % 2 !== 0
-            }),
-            map((n: number) => n * n)
-        );
-        timerObserver.subscribe(x => {
-            this.count = x
-        });
-    }
 }
 </script>
